debec81e68be6e5e4b442e1156021a9ec5dc0bd8
Boun702 <Blacknigit1873@gmail.com>
2025-01-17 02:36:19 +0700

    Update jc.js

--- jc.js
+++ jc.js
@@ -6,6 +6,6 @@ function checkAge() {
     if (age >= 18) {
         document.getElementById("result").innerHTML ="  <button onclick=\"window.location.href='https://www.youtube.com/watch?v=dQw4w9WgXcQ';\">ທ່ານກົງກັບເງື່ອນໄຂ click me!!</button>"
     } else {
-        document.getElementById("result").innerHTML = "ທ່ານຍັງເປັນເດັກນ້ອຍ!";
+        document.getElementById("result").innerHTML = "ອາຍຸຂອງທ່ານຍັງບໍ່ຕົງກັບເງື່ອນໄຂ ກະລຸນາລໍຖ້າຈົນກວ່າອາຍຸຂອງທ່ານຈະເຖິງ 18 ປີ";
     }
 }
